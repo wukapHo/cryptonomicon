9800f59983b9b3c4e4dbaaba6f7dd8a30174eecd
Vyacheslav Likashkov <xb0ksx@gmail.com>
2022-03-10 15:30:56 +0300

    feat: add auto-completion when adding ticker

--- src/components/AddTicker.vue
+++ src/components/AddTicker.vue
@@ -6,16 +6,30 @@
     >
       Тикер
     </label>
-    <input
-      v-model="ticker"
-      @blur="isAdded = false"
-      @keydown.enter="addTicker"
-      @input="suggestAutoCompletion"
-      class="ticker__input"
-      id="ticker"
-      type="text"
-      placeholder="Например DOGE"
-    >
+    <div class="">
+      <input
+        v-model="ticker"
+        @blur="isAdded = false"
+        @keydown.enter="addTicker(ticker)"
+        class="ticker__input"
+        id="ticker"
+        type="text"
+        placeholder="Например BTC"
+      >
+      <div
+        class="ticker__auto"
+        :class="{
+          'ticker__auto--active': autoCompletionList.length > 0,
+        }"
+      >
+        <div
+          v-for="(item, idx) in autoCompletionList"
+          :key="idx"
+          class="ticker__auto-item"
+          @click="addTicker(item)"
+        >{{ autoCompletionList[idx] }}</div>
+      </div>
+    </div>
     <div
       :class="{
         'ticker__error--active': isAdded,
@@ -59,46 +73,49 @@ export default {
     };
   },
 
+  computed: {
+    autoCompletionList() {
+      let data = [];
+      if (this.ticker !== '') {
+        this.coinList.forEach((coin) => {
+          if (coin.indexOf(this.ticker.toUpperCase()) === 0) {
+            data = [...data, coin];
+            if (data.length > 4) {
+              data = data.slice(0, 4);
+            }
+          }
+        });
+      }
+      return data;
+    },
+  },
+
   created() {
     this.getCoinList();
   },
 
   methods: {
-    addTicker() {
-      if (this.ticker.length === 0) return;
-      if (this.tickers.find((t) => t.name.toUpperCase() === this.ticker.toUpperCase())) {
+    addTicker(tickerName) {
+      if (tickerName.length === 0) return;
+      if (this.tickers.find((t) => t.name.toUpperCase() === tickerName.toUpperCase())) {
         this.isAdded = true;
         return;
       }
 
       // eslint-disable-next-line no-restricted-syntax
-      if (this.coinList.find((coin) => coin === this.ticker.toUpperCase())) {
+      if (this.coinList.find((coin) => coin === tickerName.toUpperCase())) {
         this.isInvalid = false;
       } else {
         this.isInvalid = true;
       }
 
-      this.$emit('add-ticker', { ticker: this.ticker, isInvalid: this.isInvalid });
+      this.$emit('add-ticker', { ticker: tickerName, isInvalid: this.isInvalid });
       this.ticker = '';
     },
     async getCoinList() {
       const coinData = await getCoinListFromApi();
-      // eslint-disable-next-line prefer-destructuring
+      // [5][1] to get a list of currencies from the api
       this.coinList = Object.keys(Object.entries(coinData)[5][1]);
-      // console.log(this.coinList);
-    },
-    suggestAutoCompletion() {
-      // eslint-disable-next-line max-len
-      // const autoCompletionList = this.coinList.filter((c) => c[1].FullName.indexOf(this.ticker) === 0);
-      // console.log(autoCompletionList);
-      // console.log(autoCompletionList.map((item) => item[1].Symbol));
-
-      // eslint-disable-next-line no-restricted-syntax
-      // this.coinList.forEach((coin) => {
-      //   // if (coin.indexOf(this.ticker.toUpperCase()) === 0) {
-      //   //   console.log(coin);
-      //   // }
-      // });
     },
   },
 
@@ -112,8 +129,8 @@ export default {
 
 <style lang="scss">
 .ticker {
-  padding: 20px;
   display: flex;
+  padding: 20px;
   flex-direction: column;
   gap: 20px;
 
@@ -129,14 +146,43 @@ export default {
     height: 30px;
     width: 220px;
     padding: 0 15px;
-    border: 2px solid transparent;
-    outline: none;
-    border-radius: 15px;
     font-size: 16px;
+    border: 2px solid transparent;
+    border-radius: 15px;
+    outline: none;
     box-shadow: 0px 0px 19px -1px rgba(34, 60, 80, 0.13);
 
     &:focus {
       border: 2px solid #cccccc;
+    }
+  }
+
+  &__auto {
+    margin-top: 5px;
+    display: flex;
+    width: 220px;
+    height: 20px;
+    padding: 0 10px;
+    // opacity: 0;
+
+    // &--active {
+    //   opacity: 1;
+    // }
+  }
+
+  &__auto-item {
+    margin-right: 5px;
+    padding: 0 5px;
+    color: #ffffff;
+    font-size: 14px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    background-color: #cccccc;
+    cursor: pointer;
+    transition: 0.3s;
+
+    &:hover {
+      background-color: #798d87;
     }
   }
 
@@ -157,13 +203,13 @@ export default {
     width: 150px;
     height: 40px;
     padding: 0 20px;
-    border: none;
-    outline: none;
-    border-radius: 20px;
-    background: #cccccc url(../assets/plus-icon.svg) no-repeat center left 20px;
     color: #ffffff;
     font-size: 18px;
     text-align: right;
+    border: none;
+    border-radius: 20px;
+    outline: none;
+    background: #cccccc url(../assets/plus-icon.svg) no-repeat center left 20px;
     cursor: pointer;
     transition: 0.3s;
 
